c626c203340062cafc321041009fdce798ff92d1
Rishabh Joshi <rishabh9@gmail.com>
2018-01-03 02:26:14 +0530

    FEAT: Add SES

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a nodejs function to be run on AWS Lambda. It accepts form data, validates recaptcha and then sends the form data to the configured email asddress.
+
--- handler.js
+++ handler.js
@@ -1,34 +1,68 @@
 'use strict';
 
-import config from './config.json';
+const config  = require('./config.json');
 const request = require('request');
 const qs      = require('querystring');
+const AWS     = require('aws-sdk');
+
+var sesClient = new AWS.SES();
 
 module.exports.form = (event, context, callback) => {
-  var body = qs.parse(event.body);
+  console.log(event);
 
-  request.post(
+  var formData = qs.parse(event.body);
+
+  request.post( 
     {
       "url"  : "https://www.google.com/recaptcha/api/siteverify",
       "form" : 
         {
               "secret"   : config.captchaSiteSecret,
-              "response" : body['g-recaptcha-response'],
+              "response" : formData['g-recaptcha-response'],
               "remoteip" : event.requestContext.identity.sourceIp
         }
     }, 
     function(error, httpResponse, body) {
-      console.log("statusCode: " + httpResponse.statusCode);
-      console.log("success: " + JSON.parse(body).success);
-      console.log("error: "  + error);
-      if (!error && httpResponse.statusCode == 200 && body.success) {
-        // Print out the response body
-        console.log("Recaptcha verified! Response was: " + body);
-        console.log("Sending email...");
+      console.log("httpResponse: " + httpResponse);
+      console.log("body: " + JSON.parse(body));
+      console.log("error: " + error);
+
+      if (!error && httpResponse.statusCode == 200 && JSON.parse(body).success) {
+
+        console.log("Recaptcha verified!");
+        console.log("Now sending email...");
+
+        var emailParams = {
+          "Destination": {
+            "ToAddresses": [ config.sendMailsTo ]
+          },
+          "Message": {
+            "Body": {
+              "Text": {
+                "Data": formData['message']
+              }
+            },
+            "Subject": {
+              "Data": formData['name'] + " has left you a message on www.rishabh.me"
+            }
+          },
+          "Source": config.sendMailsTo,
+          "ReplyToAddresses": [ formData['_replyto'] ]
+        };
+        
+        sesClient.sendEmail(emailParams, function (err, data) {
+          if (err) {
+            console.log("An error occurred while sending the email. Error: " + err);
+            callback(null, {"success": false, "message": "An error occurred sending the email!", "error": err});
+          } else {
+            console.log("Email sent successfully!");
+            callback(null, {"success": true});
+          }
+        });
 
       } else {
         console.log("Error verifying recaptcha. Response was: " + body);
-        callback(error);
+        callback(null, {"success": false, "message": "Error verifying recaptcha. You might be a robot!", "error": error});
       }
     }
   );
